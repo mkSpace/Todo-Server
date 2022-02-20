aebdcf3379e7ff8aa9424d758ecbce037d15f255
Jaemin <once29@naver.com>
2022-02-21 00:50:39 +0900

    Add sample api for checking successful connection from RDS

--- src/main/kotlin/com/funin/todo/controller/WebRestController.kt
+++ src/main/kotlin/com/funin/todo/controller/WebRestController.kt
@@ -1,14 +1,25 @@
 package com.funin.todo.controller
 
+import com.funin.todo.entity.User
+import com.funin.todo.repository.UserRepository
 import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class WebRestController(private val environment: Environment) {
+class WebRestController(
+    private val environment: Environment,
+    private val userRepository: UserRepository
+) {
 
     @GetMapping("/profile")
     fun getProfile(): String {
         return environment.activeProfiles.firstOrNull() ?: ""
+    }
+
+    @GetMapping("/funin")
+    fun test(@RequestParam username: String): User {
+        return userRepository.save(username)
     }
 }
