c5b987c6887974ce95b0ae8f65bfc4b222a0931d
Jaemin <once29@naver.com>
2022-03-10 15:41:37 +0900

    Remove unnecessary codes

--- src/main/kotlin/com/funin/todo/presentation/WebRestController.kt
+++ src/main/kotlin/com/funin/todo/presentation/WebRestController.kt
@@ -1,25 +1,16 @@
 package com.funin.todo.presentation
 
-import com.funin.todo.domain.user.User
-import com.funin.todo.domain.user.UserRepository
 import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class WebRestController(
     private val environment: Environment,
-    private val userRepository: UserRepository
 ) {
 
     @GetMapping("/profile")
     fun getProfile(): String {
         return environment.activeProfiles.firstOrNull() ?: ""
-    }
-
-    @GetMapping("/funin")
-    fun test(@RequestParam username: String): User {
-        return userRepository.save(username)
     }
 }
