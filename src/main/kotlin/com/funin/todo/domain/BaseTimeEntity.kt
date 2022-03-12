183b16f18292013153d7087fe6e72f7e51cd1ba5
Jaemin.Park <once29@naver.com>
2022-03-13 03:20:57 +0900

    Fix broken UserTest

--- src/main/kotlin/com/funin/todo/domain/BaseTimeEntity.kt
+++ src/main/kotlin/com/funin/todo/domain/BaseTimeEntity.kt
@@ -9,7 +9,7 @@ import javax.persistence.MappedSuperclass
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
-open class BaseTimeEntity {
+abstract class BaseTimeEntity {
 
     @CreatedDate
     lateinit var createdAt: LocalDateTime
