5fa4f0360d4400f87da875f7945a4a9ec812d7c7
Jaemin.Park <once29@naver.com>
2022-03-18 02:55:33 +0900

    Fix UserController to emit access token

--- src/main/kotlin/com/funin/todo/presentation/api/user/UserController.kt
+++ src/main/kotlin/com/funin/todo/presentation/api/user/UserController.kt
@@ -1,5 +1,6 @@
 package com.funin.todo.presentation.api.user
 
+import com.funin.todo.application.TokenService
 import com.funin.todo.domain.user.UserService
 import com.funin.todo.presentation.api.ApiResponse
 import com.funin.todo.presentation.api.ApiV1
@@ -8,13 +9,19 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 
 @ApiV1
-class UserController(private val userService: UserService) {
+class UserController(
+    private val userService: UserService,
+    private val jwtService: TokenService<Long>
+) {
 
     @PostMapping("/signup")
     fun signup(@RequestBody @Validated request: SignUpRequest): ApiResponse<SignUpResponse> {
         if (request.nickname.isNullOrBlank()) throw IllegalArgumentException("nickname must not be blank")
         if (request.password.isNullOrBlank()) throw IllegalArgumentException("password must not be blank")
-        val response = userService.join(request.nickname, request.password).toSignUnResponse()
-        return ApiResponse.success(response)
+        val userVO =
+            userService.join(request.nickname, request.password) ?: throw IllegalStateException("cannot create user")
+        return ApiResponse.success(
+            data = SignUpResponse(jwtService.encode(userVO.id), userVO.nickname)
+        )
     }
 }
