28b621b1642b02b696709e2e49b126248cedf6a5
Jaemin.Park <once29@naver.com>
2022-03-18 02:44:08 +0900

    Implement JasyptConfig

---
+++ src/main/kotlin/com/funin/todo/infrastructure/JasyptConfig.kt
@@ -0,0 +1,9 @@
+package com.funin.todo.infrastructure
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableEncryptableProperties
+class JasyptConfig {
+}
