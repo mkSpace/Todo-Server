a061caff43596df750673bec8e71e98c4f43da4d
Jaemin.Park <once29@naver.com>
2022-05-05 23:58:52 +0900

    Create TodoServiceTest

---
+++ src/test/kotlin/com/funin/todo/domain/todo/TodoServiceTest.kt
@@ -0,0 +1,86 @@
+package com.funin.todo.domain.todo
+
+import com.funin.todo.ActiveTestProfile
+import com.funin.todo.domain.exception.UserNotFoundException
+import com.funin.todo.domain.user.User
+import com.funin.todo.domain.user.UserRepository
+import com.funin.todo.generateSimpleAbsoluteLong
+import com.funin.todo.generateSimpleString
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentMatchers.any
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.junit.jupiter.MockitoExtension
+import java.time.LocalDateTime
+import java.util.*
+
+@ExtendWith(MockitoExtension::class)
+@ActiveTestProfile
+class TodoServiceTest {
+
+    @Mock
+    lateinit var userRepository: UserRepository
+
+    @Mock
+    lateinit var todoRepository: TodoRepository
+
+    @InjectMocks
+    lateinit var todoService: TodoServiceImpl
+
+    @Test
+    fun create_성공() {
+        // given
+        val todoId = generateSimpleAbsoluteLong()
+        val content = generateSimpleString()
+        val authorId = generateSimpleAbsoluteLong()
+        val simpleUser = User().apply { id = authorId }
+        val state = State.NONE
+        val reason = generateSimpleString()
+        val canUpdate = true
+        val simpleTodo = Todo().apply {
+            this.id = todoId
+            this.content = content
+            this.author = simpleUser
+            this.state = state
+            this.reason = reason
+            this.canUpdate = canUpdate
+            this.createdAt = LocalDateTime.now()
+        }
+
+        `when`(userRepository.findById(authorId)).thenReturn(Optional.of(simpleUser))
+        `when`(todoRepository.save(any())).thenReturn(simpleTodo)
+
+        // when
+        val todoVO = todoService.create(authorId, content, reason, state, canUpdate)
+
+        // then
+        verify(todoRepository).save(any())
+        assertThat(todoVO).isNotNull
+        assertThat(todoVO?.content).isEqualTo(content)
+        assertThat(todoVO?.authorId).isEqualTo(authorId)
+        assertThat(todoVO?.state).isEqualTo(state)
+    }
+
+    @Test
+    fun create_유저를_찾을수없음() {
+        // given
+        val todoId = generateSimpleAbsoluteLong()
+        val content = generateSimpleString()
+        val authorId = generateSimpleAbsoluteLong()
+        val state = State.NONE
+        val reason = generateSimpleString()
+        val canUpdate = true
+
+        `when`(userRepository.findById(authorId)).thenReturn(Optional.empty())
+
+        // then
+        assertThrows<UserNotFoundException> {
+            todoService.create(authorId, content, reason, state, canUpdate)
+        }
+    }
+}
